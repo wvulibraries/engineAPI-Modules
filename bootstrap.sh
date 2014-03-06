70ee414ad8a2e972214fdeba115be7de5cb11ec6
David Gersting <dgersting@gmail.com>
2014-03-06 13:38:13 -0500

    Include webmin inside Vagrant box

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,7 +15,7 @@ yum -y install mysql-connector-java mysql-connector-odbc mysql-devel mysql-lib m
 #yum -y install mod_auth_kerb mod_auth_mysql mod_authz_ldap mod_evasive mod_perl mod_security mod_ssl mod_wsgi
 yum -y install php php-bcmath php-cli php-common php-gd php-ldap php-mbstring php-mcrypt php-mysql php-odbc php-pdo php-pear php-pear-Benchmark php-pecl-apc php-pecl-imagick php-pecl-memcache php-soap php-xml php-xmlrpc php-phpunit-PHPUnit* php-phpunit-DbUnit
 yum -y install emacs emacs-common emacs-nox nano
-yum -y install git wget
+yum -y install git wget http://prdownloads.sourceforge.net/webadmin/webmin-1.670-1.noarch.rpm
 
 #mv /etc/httpd/conf.d/mod_security.conf /etc/httpd/conf.d/mod_security.conf.bak
 /etc/init.d/httpd start
