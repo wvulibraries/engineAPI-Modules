b755b564d2efc7b0245cf1a9a8e55e6459dd787d
Scott Blake <Scott.Blake@mail.wvu.edu>
2014-03-20 21:08:28 -0400

    Started createProcessor testing

--- tests/modules/form/formProcessorTest.php
+++ tests/modules/form/formProcessorTest.php
@@ -1,7 +1,23 @@
 <?php
 class formProcesserTest extends PHPUnit_Framework_TestCase {
-	function testFoo(){
-		$this->assertTrue(true);
+	private $db;
+	private $processor;
+
+	function setUp() {
+		$this->db        = db::get('appDB')->query(file_get_contents(__DIR__.'/fieldBuilderTest.sql'));
+		$this->processor = new formProcessor('fieldBuilderTest', $this->db);
+	}
+
+
+	function test_setProcessorType(){
+		$this->processor->setProcessorType('insert');
+		$this->assertAttributeEquals(formBuilder::TYPE_INSERT, 'processorType', $this->processor);
+
+		$this->processor->setProcessorType('update');
+		$this->assertAttributeEquals(formBuilder::TYPE_UPDATE, 'processorType', $this->processor);
+
+		$this->processor->setProcessorType('edit');
+		$this->assertAttributeEquals(formBuilder::TYPE_EDIT,   'processorType', $this->processor);
 	}
 }
- 
+
