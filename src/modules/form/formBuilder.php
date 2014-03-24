eb8a36f560b6cd4e467c6260094344dd85cb534d
David Gersting <dgersting@gmail.com>
2014-03-24 14:36:27 -0400

    Set the default formBuilder timeout to 24hr

--- src/modules/form/formBuilder.php
+++ src/modules/form/formBuilder.php
@@ -2,7 +2,7 @@
 
 class formBuilder{
 	const DEFAULT_FORM_NAME       = '';
-	const DEFAULT_FORM_TIMEOUT    = 900;
+	const DEFAULT_FORM_TIMEOUT    = 86400;
 	const SESSION_SAVED_FORMS_KEY = 'formBuilderForms';
 	const TYPE_UNKNOWN = 0;
 	const TYPE_INSERT  = 1;
